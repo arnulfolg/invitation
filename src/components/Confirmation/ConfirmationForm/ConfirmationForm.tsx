9e78952a37102bf0b41a2f8b33b36d0031c36d55
Arnulfo Loredo <arnulfo.lg@live.com>
2023-05-17 23:21:17 -0700

    Added more sections and confirmation form

---
+++ src/components/Confirmation/ConfirmationForm/ConfirmationForm.tsx
@@ -0,0 +1,85 @@
+import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
+
+
+interface FormValues {
+	fullName: string,
+	confirmation: string
+}
+
+export default function ConfirmationForm() {
+	
+	const wpNumberLink: string = `https://wa.me/528331063870?text=`;
+
+	const ConfirmationSchema = Yup.object().shape({
+		fullName: Yup.string()
+			.min(6, '¿Tu nombre tal vez es muy corto? Intenta con tu nombre completo.')
+			.max(100, '¡Oh! Esto es inesperado, tal vez tu nombre es muy largo.')
+			.required('Por favor ingresa tu nombre completo.'),
+		confirmation: Yup.string().oneOf(['true', 'false'])
+			.required('Por favor confirma si podrás asistir.')
+	});
+
+	const onSubmitAction = (values: FormValues) => {
+		let newConfirmationMessage: string = '';
+
+			if(values.confirmation === 'true') {
+				newConfirmationMessage = ` ${wpNumberLink}
+					¡Hola!, confirmo que Sí voy a asistir a la boda de Arnulfo y Caissa.
+					\nMi nombre es: ${values.fullName}`
+			} else {
+				newConfirmationMessage =  ` ${wpNumberLink}
+					Hola, mi nombre es ${values.fullName}.
+					\nLamento confirmar que no podré asistir a la boda de Arnulfo y Caissa.`
+			}
+
+			window.open(newConfirmationMessage)
+	}
+
+	const FormInitialValues: FormValues = {
+		fullName: '',
+		confirmation: '',
+	}
+
+	return (
+	<Formik
+		initialValues={FormInitialValues}
+		validationSchema={ConfirmationSchema}
+		onSubmit={onSubmitAction}
+		>
+		{({ errors, touched }) => (
+			<Form className='form_group'>
+				<section className='form_section'>
+					<label className='form_input_title' htmlFor="fullName">Nombre y Apellido</label>
+					<Field type="text" id="fullName" name="fullName"/>
+					{errors.fullName && touched.fullName ? (
+						<span className='form_section__error'>{errors.fullName}</span>
+					) : null}
+				</section>
+
+				<fieldset className='form_section'>
+					<legend className='form_input_title'>¿Podrás asistir a la boda?</legend>
+					<section className='form_section_radio'>
+						<Field type="radio" id="si" name="confirmation" value="true"/>
+						<label htmlFor="si">Sí</label>
+					</section>
+					<section className='form_section_radio'>
+						<Field type="radio" id="no" name="confirmation" value="false"/>
+						<label htmlFor="no">No</label>
+					</section>
+					{errors.confirmation && touched.confirmation ? (
+						<span className='form_section__error'>{errors.confirmation}</span>
+					) : null}
+				</fieldset>
+
+				<section className='form_section'>
+					<button className='button' type="submit">Confirmar asistencia por WhatsApp</button>
+				</section>
+
+			</Form>
+		)}
+
+    </Formik>
+  )
+}
+
--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,38 +1,38 @@
 import './timer.css'
 import dayjs from 'dayjs'
 
-
-var d1: any = new Date(); //"now"
-var d2: any = new Date("2023-10-19");  // some date
-var diff = Math.abs(d1-d2);  // difference in milliseconds
-
-const date = dayjs(diff);
-
-const date_month = date.format('M')
-const date_day = date.format('D')
-const date_hours = date.format('H')
-
 export default function Timer() {
-  return (
-	<section className="hero_date_timer">
-		<p className='small'>Estamos a:</p>
-		<p className="timer">
-			<span className="date_month">
-				{date_month}
-				<span className="small">meses</span>
-			</span>
-			<span className="">:</span>
-			<span className="date_day">
-				{date_day}
-				<span className="small">días</span>
-			</span>
-			<span className="">:</span>
-			<span className="date_hours">
-				{date_hours}
-				<span className="small">horas</span>
-			</span>
-		</p>
-	</section>
-  )
+	
+	const d1: any = new Date(); //"now"
+	const d2: any = new Date("2023-10-19");  // some date
+	
+	const diff = Math.abs(d1-d2);  // difference in milliseconds
+	const date = dayjs(diff);
+	
+	const date_month = date.format('M')
+	const date_day = date.format('D')
+	const date_hours = date.format('H')
+
+	return (
+		<section className="hero_date_timer">
+			<p className='small'>Estamos a:</p>
+			<p className="timer">
+				<span className="date_month">
+					{date_month}
+					<span className="small">meses</span>
+				</span>
+				<span className="">:</span>
+				<span className="date_day">
+					{date_day}
+					<span className="small">días</span>
+				</span>
+				<span className="">:</span>
+				<span className="date_hours">
+					{date_hours}
+					<span className="small">horas</span>
+				</span>
+			</p>
+		</section>
+	)
 }
 
