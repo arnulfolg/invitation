c193ed416e66c7562d37237c7072ec45702f9ac5
Arnulfo Loredo <arnulfo.lg@live.com>
2025-08-19 23:16:11 -0400

    Updated Tailwind to V4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   site: "https://arnulfoycaissa.com",
-  integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    config: {
-      applyBaseStyles: false
-    }
-  }), react()]
+
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
